3edb4861d322901dc280ef20da7c0ecedbdafc1b
user123 <mo7amedfares321@gmail.com>
2021-07-19 17:39:03 +0200

    sadasdwryhw123

--- script.js
+++ script.js
@@ -3,6 +3,39 @@ const app = express()
 const bodyparser = require("body-parser")
 const cors = require("cors")
 const PORT = process.env.PORT 
+app.use(express.json())
+app.use(cors())
+// const { Client } = require('pg');
+
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+// client.connect();
+
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
+// client.connect();
+
+//     client.query('SELECT table_schema,* FROM information_schema.tables;', (err, res) => {
+//       if (err) throw err;
+//       for (let row of res.rows) {
+//         console.log(JSON.stringify(row));
+//       }
+//       client.end();
+// });
+
+
+
+
 const knex = require('knex')({
     client: 'pg',
     connection: {
@@ -10,10 +43,8 @@ const knex = require('knex')({
       ssl:true,
     }
 });
-app.use(express.json())
-app.use(cors())
 app.get("/",(req,resp)=>{
-    resp.json(process.env.DATABASE_URL)
+    resp.json(knex("users"))
 })
 app.post("/register",(req,resp)=>{
     var email = req.body.email;
